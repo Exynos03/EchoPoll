fb9288021c44142183fd536329104a0a7a6cc240
Exynos03 <sumitghosh345@gmail.com>
2025-01-22 23:56:06 +0530

    routine commit

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello Turbo")
+import http from 'http'
+
+async function main() {
+    const httpServer = http.createServer()
+
+    const PORT = 8080
+
+    httpServer.listen(PORT, () => {
+        console.log(`Server is up and running on ${PORT}`)
+    })
+}
