29487f4cc2118ff32887ad41d679da39c9908bdf
Exynos03 <sumitghosh345@gmail.com>
2025-01-31 14:11:32 +0530

    redis setup done with the server

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,11 +1,18 @@
 import http from 'http'
+import SocketService from './services/socket'
 
 async function main() {
+    const socketService = new SocketService()
     const httpServer = http.createServer()
 
     const PORT = 8080
 
+    socketService.io.attach(httpServer)
     httpServer.listen(PORT, () => {
         console.log(`Server is up and running on ${PORT}`)
     })
+
+    socketService.initListeners()
 }
+
+main()
--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,11 +1,51 @@
 import { Server } from "socket.io"
+import Redis from "ioredis";
+import { json } from "stream/consumers";
+import { channel } from "diagnostics_channel";
+
+const pub = new Redis({
+    host: 'caching-1bc93745-sumitghosh345-7eac.d.aivencloud.com',
+    port: 21188,
+    username: 'default',
+    password: 'AVNS_icGwyX3hjKpv-A10aPe'
+})
+const sub = new Redis({
+    host: 'caching-1bc93745-sumitghosh345-7eac.d.aivencloud.com',
+    port: 21188,
+    username: 'default',
+    password: 'AVNS_icGwyX3hjKpv-A10aPe'
+})
 
 class SocketService {
     private _io: Server;
     
     constructor() {
         console.log("Init socket service...")
-        this._io = new Server()
+        this._io = new Server({
+            cors: {
+                allowedHeaders: ["*"],
+                origin: "*",
+            }
+        })
+        sub.subscribe("MESSAGES")
+    }
+
+    public initListeners() {
+        const io = this._io
+        console.log("Init Socket listeners...")
+        io.on("connect", (socket) => {
+            console.log("Client connected : ", socket.id)
+        
+            socket.on('event:message', async ({ message }: {message: string}) => {
+                await pub.publish("MESSAGES", JSON.stringify({message}))
+            })
+        })
+
+        sub.on('message', async (channel, msg) => {
+            if(channel === "MESSAGES") {
+                io.emit("message", msg)
+            }
+        })
     }
 
     get io() {
